fa1439d00535d31a8a311e566243dce8a8ba7851
Julian Tiu <juliantiu@MacBook-Pro.local>
2020-08-02 18:38:58 -0500

    firebase login template

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { withRouter, Redirect } from "react-router";
+import { useAuthState } from '../../context_hooks/AuthState'
+import app from "../../firebase";
 
-export default function Login() {
+const Login = ({ history }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [isRegister, setIsRegister] = useState(false);
+  const [invalid, setInvalid] = useState(false);
+  const { currentUser } = useAuthState();
+
+  const handleLogin = useCallback(
+    async () => {
+      try {
+        await app
+          .auth()
+          .signInWithEmailAndPassword(email, password);
+        setInvalid(false);
+        history.push('/');
+      } catch (error) {
+        setInvalid(true);
+        throw error;
+      }
+    },
+    [history, email, password, setInvalid]
+  );
+
+  if (currentUser) {
+    return <Redirect to="/" />;
+  }
+
   return (
     <h1>Hello World</h1>
   )
 }
+
+export default withRouter(Login);
